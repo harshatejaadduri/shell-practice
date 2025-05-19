bedac3c007862d1e1db33cb8beba58bacafac4ab
harshatejaadduri <harshateja3@gmail.com>
2025-05-19 12:19:29 +0530

    Updated

---
+++ 03.variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+P1=Sarah
+P2=Mike
+
+echo "P1: Hey, P2! It’s been so long since we last met. How have you been?"
+echo "P2: I know, right? I’ve been good, just busy with work. What about you?"
+echo "P1: Same here! Work’s been a bit hectic. I just got back from a small trip to the mountains."
+echo "P2: That sounds amazing! Did you hike?"
+echo "P1: Yes, we did! The views were incredible, and it was nice to disconnect for a bit."
+echo "P2: I love it! We should plan a hiking trip together next time."
+echo "P1: Absolutely! Let’s do it soon!"
