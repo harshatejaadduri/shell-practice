01b1765926249a86350ab15d57b1038ecce7f5f5
harshatejaadduri <harshateja3@gmail.com>
2025-05-19 12:15:18 +0530

    Updated

---
+++ 02.print.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Sarah: Hey, Mike! It’s been so long since we last met. How have you been?"
+echo "Mike: I know, right? I’ve been good, just busy with work. What about you?"
+echo "Sarah: Same here! Work’s been a bit hectic. I just got back from a small trip to the mountains."
+echo "Mike: That sounds amazing! Did you hike?"
+echo "Sarah: Yes, we did! The views were incredible, and it was nice to disconnect for a bit."
+echo "Mike: I love it! We should plan a hiking trip together next time."
+echo "Sarah: Absolutely! Let’s do it soon!"
