4b20dc44814099afbf8292cbacda2951239b7e47
harshatejaadduri <harshateja3@gmail.com>
2025-05-20 17:57:18 +0530

    Updated

---
+++ 11.install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ #to install any application we should have root access 
+ #we give condition that user id=0 as root id is 0
+
+ USERID=$(id -u)
+
+ if [ $USERID -ne 0 ]
+
+ then  
+
+      echo "You dont have root permissions"
+# here shell doesnt end even after the condition is false and proceeds to install
+# so we use exit code 0 for success and 1-127 is failure
+    exit 1
+#here userid !=0 so it prints "You dont have root permissions" and ends @exit code 1
+
+ fi
+ 
+ dnf install nginx -y
+
+if [ $? -eq 0 ]
+
+then 
+    echo "Installation is ....Successful"
+else 
+    echo "Installation is ....Failure"
+    exit 1
+fi
