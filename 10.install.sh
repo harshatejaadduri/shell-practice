1494eb7da7c14ae73b9a6bbb1fbe71fbae30771b
harshatejaadduri <harshateja3@gmail.com>
2025-05-20 17:33:23 +0530

    Updated

---
+++ 10.install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ #to install any application we should have root access 
+ #we give condition that user id=0 as root id is 0
+
+ USERID=$(id -u)
+
+ if [ USERID -ne 0]
+
+ then  
+
+      echo "You dont have root permissions"
+else 
+
+    dnf install nginx -y
+
+fi
